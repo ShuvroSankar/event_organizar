78154dc2c580b49d6f3a56e5130a22a0e7322907
ShuvroSankar <sankarshuvro1010@gmail.com>
2025-03-07 12:07:03 +0600

    tailwind css setup, declation of primary color

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,47 @@
+import { Container } from "lucide-react";
+
 const config = {
   plugins: ["@tailwindcss/postcss"],
+  theme:{
+    Container:{
+      center: true,
+      padding:"15px"
+    },
+    screens:{
+      sm:"640px",
+      md:"768px",
+      lg:"1024px",
+      xl:"1310px",
+    },
+    fontFamily:{
+
+    },
+    extends:{
+      colors:{
+        primary:"#0f1017",
+        accent:{
+          DEFAULT:"#e14817",
+          hover:"#cb4419",
+        },
+        secondary:{
+          DEFAULT:"#1b1c23",
+          hover:"#1f212b",
+        },
+        tertiary:{
+          DEFAULT:"#26272e",
+          hover:"#24252c",
+        },
+        grey:"#737373",
+      },
+      backgroundImage:{
+        hero_bg1:"url(/assets/hero/hero-bg1.png)",
+        hero_bg2:"url(/assets/hero/hero-bg2.png)",
+        pattern:"url(/assets/pattern_bg.png)",
+      },
+
+    }
+  }
 };
 
 export default config;
+
